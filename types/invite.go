db60087bdef1b85b0cfc0782d431f2ccea734118
Robert Bruce <entegral>
2023-12-05 13:25:37 -0800

    comment invite

--- types/invite.go
+++ types/invite.go
@@ -8,24 +8,20 @@ import (
 
 	"github.com/entegral/toolbox/clients"
 	"github.com/entegral/toolbox/dynamo"
-	"github.com/entegral/toolbox/helpers"
 )
 
+// Invite is a struct representing an invitation.
 type Invite struct {
-	dynamo.DiLink[*Event, *User]
-	Status InviteStatus `json:"status"`
+	dynamo.DiLink[*Event, *User]              // DiLink is a struct from the dynamo package that links two entities.
+	Status                       InviteStatus `json:"status"` // Status represents the status of the invitation.
 }
 
+// Type method returns the type of the struct, in this case "invite".
 func (i Invite) Type() string {
 	return "invite"
 }
 
-func (i *Invite) Link(ctx context.Context, clients clients.Client) error {
-	_, err := helpers.PutItem(ctx, i)
-	return err
-}
-
-// User returns the user associated with the invite.
+// User method loads the user entity associated with the invite.
 func (i *Invite) User(ctx context.Context, clients clients.Client) (*User, error) {
 	_, err := i.LoadEntity1(ctx, clients)
 	if err != nil {
@@ -34,7 +30,7 @@ func (i *Invite) User(ctx context.Context, clients clients.Client) (*User, error
 	return i.Entity1, nil
 }
 
-// Event returns the event associated with the invite.
+// Event method loads the event entity associated with the invite.
 func (i *Invite) Event(ctx context.Context, clients clients.Client) (*Event, error) {
 	_, err := i.LoadEntity0(ctx, clients)
 	if err != nil {
@@ -43,12 +39,12 @@ func (i *Invite) Event(ctx context.Context, clients clients.Client) (*Event, err
 	return i.Entity0, nil
 }
 
-// NewInviteOpts is a type for defining options for creating a new invite.
+// NewInviteOpts is a struct that holds options for creating a new invite.
 type NewInviteOpts struct {
-	Status *InviteStatus
+	Status *InviteStatus // Status is a pointer to an InviteStatus value.
 }
 
-// NewInvite creates a new invite.
+// NewInvite is a function that creates a new invite. It requires an event, a user, and options for the invite.
 func NewInvite(ctx context.Context, event Event, user User, opts *NewInviteOpts) (*Invite, error) {
 	if event.CreatedByEmail == "" {
 		return nil, fmt.Errorf("event must have a CreatedByEmail")
@@ -66,20 +62,24 @@ func NewInvite(ctx context.Context, event Event, user User, opts *NewInviteOpts)
 	return invite, err
 }
 
+// InviteStatus is a type representing the status of an invitation.
 type InviteStatus string
 
+// Constants representing possible statuses of an invitation.
 const (
 	InviteStatusAccepted InviteStatus = "ACCEPTED"
 	InviteStatusDeclined InviteStatus = "DECLINED"
 	InviteStatusPending  InviteStatus = "PENDING"
 )
 
+// AllInviteStatus is a slice containing all possible statuses of an invitation.
 var AllInviteStatus = []InviteStatus{
 	InviteStatusAccepted,
 	InviteStatusDeclined,
 	InviteStatusPending,
 }
 
+// IsValid method checks if the InviteStatus value is valid.
 func (e InviteStatus) IsValid() bool {
 	switch e {
 	case InviteStatusAccepted, InviteStatusDeclined, InviteStatusPending:
@@ -88,10 +88,12 @@ func (e InviteStatus) IsValid() bool {
 	return false
 }
 
+// String method returns the InviteStatus value as a string.
 func (e InviteStatus) String() string {
 	return string(e)
 }
 
+// UnmarshalGQL method is used to unmarshal a GraphQL value into an InviteStatus value.
 func (e *InviteStatus) UnmarshalGQL(v interface{}) error {
 	str, ok := v.(string)
 	if !ok {
@@ -105,6 +107,7 @@ func (e *InviteStatus) UnmarshalGQL(v interface{}) error {
 	return nil
 }
 
+// MarshalGQL method is used to marshal an InviteStatus value into a GraphQL value.
 func (e InviteStatus) MarshalGQL(w io.Writer) {
 	fmt.Fprint(w, strconv.Quote(e.String()))
 }
